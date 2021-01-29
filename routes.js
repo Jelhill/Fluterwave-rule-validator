1108f102422cad02e998177399b6b7eddd52721a
Jelhill <talk2jelili2000@gmail.com>
2021-01-29 15:02:49 +0100

    Update adjustmen on rule-validator-endpoint

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 
-  let data = {
+  let personalData = {
     "name": "Abduljelili Osigbemhe Umaru",
     "github": "@jelhill",
     "email": "talk2jelili2000@gmail.com",
@@ -10,40 +10,37 @@ const router = express.Router()
     "twitter": "@jelhill"
   }
 
-valData = {
-    "rule": {
-      "field": "0",
-      "condition": "gte",
-      "condition_value": 40
-    },
-    // "data": {
-    //   "name": "James Holden",
-    //   "crew": "Rocinante",
-    //   "age": 34,
-    //   "position": "Captain",
-    //   "missions": {
-    //     "count": 45,
-    //     "successful": 44,
-    //     "failed": 1
-    //   }      
-      "data": [40, 25, "Hello"]
-    // }
-    // "data": "missions"
-      // "data": ["missions", "Hi"]
-  }
-
 router.get("/", (req, res) => {
     res.status(200)
     .json({
       message: "My Rule-Validation API", 
       status: "success", 
-      data
+      data: personalData
     })
 })
 
 router.post("/validate-rule", (req, res) => {
-  req.body = valData
-  
+
+  if(Array.isArray(req.body)) {
+    return res.json({
+      message: "Invalid JSON payload passed.",
+      status: "error",
+      data: null
+    })      
+  }
+
+
+  if(typeof req.body !== "object") {
+    if(Array.isArray(req.body)){
+      return res.json({
+        message: "Invalid JSON payload passed.",
+        status: "error",
+        data: null
+      })      
+    }
+  }
+
+
   if(req.body.rule === undefined && req.body.data === undefined) {
     return res.json({
       message: "rule and data is required.",
@@ -85,7 +82,6 @@ router.post("/validate-rule", (req, res) => {
   }
     
   let fieldToArray = req.body.rule.field.split(".")
-  let fieldType
   let field_value
   let condition = req.body.rule.condition
   let condition_value = req.body.rule.condition_value
@@ -127,20 +123,15 @@ router.post("/validate-rule", (req, res) => {
     })
     }
     
-
-    if(fieldToArray.length > 1) {
-      fieldType = "object"
-    }
-
-    if(typeof req.body.data === "string") {
-
-      
-    }
-
-
-    console.log("Options", Object.keys(req.body.data))
-    console.log("body data", req.body.data)
     if(Array.isArray(req.body.data) === false && fieldToArray.length === 1 && !Object.keys(req.body.data).includes(fieldToArray[0])) {
+      return res.status(400).json({
+        message: `field ${fieldToArray[0]} is missing from data.`,
+        status: "error",
+        data: null
+      })
+    }
+
+    if(Array.isArray(req.body.data) && fieldToArray.length === 1 && !req.body.data.includes(fieldToArray[0])) {
       return res.status(400).json({
         message: `field ${fieldToArray[0]} is missing from data.`,
         status: "error",
@@ -155,7 +146,6 @@ router.post("/validate-rule", (req, res) => {
         data: null
       })
     }
-
 
     // Check to see if the field Matches the data.field.
     if(Object.keys(req.body.data).includes(fieldToArray[0])){
@@ -172,7 +162,6 @@ router.post("/validate-rule", (req, res) => {
       }
     }
 
-
     if(fieldToArray.length === 1 && !Array.isArray(req.body.data)) {
       field_value = req.body.data[fieldToArray[0]]
     }
@@ -180,9 +169,6 @@ router.post("/validate-rule", (req, res) => {
     if(fieldToArray.length === 2 && !Array.isArray(req.body.data)) {
       field_value = req.body.data[fieldToArray[0]][fieldToArray[1]]
     }
-
-
-    console.log(field_value, condition, condition_value)
 
     let conditionPassed
     if(condition === "eq") {
@@ -193,7 +179,7 @@ router.post("/validate-rule", (req, res) => {
       field_value !== condition_value ? conditionPassed = true : conditionPassed = false
     }
 
-    if(condition === "gt") {
+      if(condition === "gt") {
       field_value > condition_value ? conditionPassed = true : conditionPassed = false
     }
 
@@ -222,7 +208,6 @@ router.post("/validate-rule", (req, res) => {
       })
     }
 
-
     res.status(200)
     .json({
       message: `field ${req.body.rule.field} successfully validated.`,
@@ -231,13 +216,12 @@ router.post("/validate-rule", (req, res) => {
         validation: {
           error: false,
           field: req.body.rule.field,
-          field_value: 45,
+          field_value: field_value,
           condition: req.body.rule.condition,
           condition_value: req.body.rule.condition_value
         }
       }
     })
-
 })
 
 
